9030245bff84c1574a3b77156c58e80dc8aa542e
AdventurousMR2020 <niallholmes2611@gmail.com>
2020-08-19 18:32:01 +0100

    Made baby gun finely fire babys

--- lua/weapons/advmr_weapon_baby/shared.lua
+++ lua/weapons/advmr_weapon_baby/shared.lua
@@ -6,3 +6,81 @@ SWEP.Spawnable = true
 
 function SWEP:Initialize()
 end
+
+SWEP.ViewModel = "models/weapons/v_IRifle.mdl"
+SWEP.WorldModel = "	models/weapons/w_IRifle.mdl"
+
+function SWEP:PrimaryAttack()
+
+    if ( CLIENT ) then return end
+    local Ent = ents.Create( "prop_physics" )
+    if ( !IsValid( Ent ) ) then return end
+    Ent:SetModel( "models/props_c17/doll01.mdl" )
+
+    Ent:SetPos( self:GetOwner():EyePos() + (self:GetOwner():GetAimVector() * 1 ) )
+    Ent:SetAngles( 
+        self:GetOwner():EyeAngles() )
+    Ent:Spawn()
+
+    local phys = Ent:GetPhysicsObject()
+	if ( !IsValid( phys ) ) then ent:Remove() return end
+
+    local velocity = 
+    self:GetOwner():GetAimVector()
+    velocity = velocity * 1000
+    velocity = velocity + (VectorRand() * 10)
+    phys:ApplyForceCenter( velocity )
+
+    cleanup.Add( 
+        self:GetOwner(), "props", Ent )
+
+    undo.Create( "Thrown_Baby" )
+        undo.AddEntity( Ent )
+        undo.SetPlayer( 
+            self:GetOwner() )
+    undo.Finish()
+
+    self.BaseClass.ShootEffects(self)
+
+    self:SetNextPrimaryFire( CurTime() + 0.25 )
+
+end
+
+
+function SWEP:SecondaryAttack()
+
+    if ( CLIENT ) then return end
+    local Ent = ents.Create( "prop_physics" )
+    if ( !IsValid( Ent ) ) then return end
+    Ent:SetModel( "models/props_c17/doll01.mdl" )
+
+    Ent:SetPos( self:GetOwner():EyePos() + (self:GetOwner():GetAimVector() * 1 ) )
+    Ent:SetAngles( 
+        self:GetOwner():EyeAngles() )
+    Ent:Spawn()
+
+    local phys = Ent:GetPhysicsObject()
+	if ( !IsValid( phys ) ) then ent:Remove() return end
+
+    local velocity = 
+    self:GetOwner():GetAimVector()
+    velocity = velocity * 5000
+    velocity = velocity + (VectorRand() * 10)
+    phys:ApplyForceCenter( velocity )
+
+    cleanup.Add( 
+        self:GetOwner(), "props", Ent )
+
+    undo.Create( "Thrown_Baby" )
+        undo.AddEntity( Ent )
+        undo.SetPlayer( 
+            self:GetOwner() )
+    undo.Finish()
+
+    self.BaseClass.ShootEffects(self)
+
+    
+    self:SetNextPrimaryFire( CurTime() + 0.25 )
+    
+
+end
