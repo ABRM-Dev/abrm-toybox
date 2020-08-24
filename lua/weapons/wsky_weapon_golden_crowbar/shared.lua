91d3d297d350e6c5df3a558a3e15c1def24bd44f
Liam Holmes <email@liamholmes.co.uk>
2020-08-24 16:55:58 +0100

    more work on crowbar, moved code for turning entity to gold into shared

--- lua/weapons/wsky_weapon_golden_crowbar/shared.lua
+++ lua/weapons/wsky_weapon_golden_crowbar/shared.lua
@@ -1,7 +1,7 @@
 DEFINE_BASECLASS("wsky_weapon_base")
 
 SWEP.PrintName = "Golden Crowbar"
-SWEP.Category = "Holmes Toybox"
+SWEP.Category = sharedCategoryName
 
 SWEP.Spawnable = true
 
@@ -12,6 +12,24 @@ SWEP.ViewModel = "models/weapons/c_crowbar.mdl"
 SWEP.Primary.Ammo = "none"
 SWEP.Secondary.Ammo = "none"
 
+modelMaterial = "models/player/shared/gold_player"
+
+function SWEP:ShootEffect()
+  print("ACT_VM_SWINGHARD")
+  self:SendWeaponAnim( ACT_VM_HITCENTER )  -- View model animation
+  self:GetOwner():SetAnimation( PLAYER_ATTACK1 )
+end
+
 function SWEP:Initialize()
-  self:SetHoldType("magic")
+  self:SetHoldType("melee2")
+  self:SetMaterial(modelMaterial)
+end
+
+function SWEP:PreDrawViewModel(vm, weapon, ply)
+  vm:SetMaterial(modelMaterial)
+end
+
+function SWEP:PrimaryAttack()
+  self:ShootEffect()
+  self:EmitSound(self.ClassName .. "/attack1.mp3", 75)
 end
