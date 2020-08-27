a7e26127495c6b70bcfa4d3e9d52692ce971d1ce
AdventurousMR2020 <niallholmes2611@gmail.com>
2020-08-27 18:22:23 +0100

    Added advmr props to abrm

---
+++ lua/entities/advmr_entities_box/init.lua
@@ -0,0 +1,44 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+function ENT:Initialize()
+
+  self:SetModel("models/props_junk/cardboard_box001a.mdl")
+  startPhysics(self)
+
+end
+
+function startPhysics(entity)
+  entity:PhysicsInit(SOLID_VPHYSICS)
+  entity:SetMoveType(MOVETYPE_VPHYSICS)
+  entity:SetSolid(SOLID_VPHYSICS)
+  
+  local phys = entity:GetPhysicsObject()
+
+  if phys:IsValid() then
+
+  phys:Wake()
+
+  end
+end
+
+function Explode(self)
+local explosion = ents.Create("env_explosion")
+explosion:SetPos(self:GetPos())
+explosion:Spawn()
+explosion:SetKeyValue("iMagnitude", "100")
+explosion:Fire("Explode", 0, 0)
+
+end
+
+function ENT:OnTakeDamage(dmg)
+  self:TakePhysicsDamage(dmg)
+  if (self.HealthAmount <= 0) then return; end
+  self.HealthAmount = self.HealthAmount - dmg:GetDamage()
+  if (self.HealthAmount <= 0) then
+    Explode(self)
+    self:Remove()
+  end
+end
---
+++ lua/entities/advmr_entities_box/shared.lua
@@ -0,0 +1,9 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+
+ENT.PrintName = "ABRM Toybox"
+ENT.Category = "AdventurousMR's Props"
+
+ENT.Spawnable = true
+
+ENT.HealthAmount = 1
